bc4cedb02a5a1e634169892f5bfb961cce1f7917
jacky <jackychh7878@gmail.com>
2025-03-28 00:33:01 +0800

    t-flow agent

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { useEffect } from 'react'
 import '@n8n/chat/style.css'
 import { createChat } from '@n8n/chat'
 import './App.css'
-import CCGLogo from './assets/CCG_Logo_V.png'
+import CCGLogo from './assets/catomind_logo.png'
 
 function App() {
   useEffect(() => {
     createChat({
-      webhookUrl: 'https://steveykyu.app.n8n.cloud/webhook/838b8a3c-76de-41ea-8a6a-da3938fe8283/chat',
+      webhookUrl: 'https://steveykyu.app.n8n.cloud/webhook/062b213b-7862-4d1d-bcdf-1dfcc963de3f/chat',
       target: '#n8n-chat-container',
       mode: 'fullscreen',
       showWelcomeScreen: true,
-      initialMessages: ['Hello! Welcome to CCG Cost Research Agent', 'How can I help you today?']
+      initialMessages: ['Hello! Welcome to T-Flow Project Management Agent', 'How can I help you today?']
     });
   }, []);
 
@@ -19,7 +19,7 @@ function App() {
     <div className="chat-app-wrapper">
       <header className="app-header">
         <img src={CCGLogo} alt="CCG Logo" className="ccg-logo" />
-        <h1 className="app-title">Cost Library Research Agent</h1>
+        <h1 className="app-title">T-Flow Project Management Agent</h1>
       </header>
         <div id="n8n-chat-container" className="full-height-chat"></div>
     </div>
